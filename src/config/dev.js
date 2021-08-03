002d83d3dc1ddd7a3ab1043ca6e22c84a559508b
Ivan Cabrera <ivancabrera931@gmail.com>
2021-08-03 15:42:28 -0300

    Env variables added

--- src/config/dev.js
+++ src/config/dev.js
@@ -2,6 +2,5 @@ export const config = {
   secrets: {
     jwt: 'learneverything'
   },
-  dbUrl:
-    'mongodb+srv://icab83:p4pp012@cluster0.fuzli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  dbUrl: process.env.MONGODB
 }
--- src/config/testing.js
+++ src/config/testing.js
@@ -2,6 +2,5 @@ export const config = {
   secrets: {
     jwt: 'learneverything'
   },
-  dbUrl:
-    'mongodb+srv://icabrera29:icabAr777@cluster0.fuzli.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+  dbUrl: process.env.MONGODB
 }
